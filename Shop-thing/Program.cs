4b63473c461ec6c3b47b7fec32ea53c0203cc62f
MckeeCMohawk <000983139@mohawkcollege.ca>
2026-01-22 16:44:11 -0500

    Implement transaction

--- Shop-thing/Program.cs
+++ Shop-thing/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Step 1: Ask player for name and remember it
 using System.ComponentModel.Design;
 
-Console.WriteLine("Hello fair traveller");
+Console.WriteLine("Hello fair traveller.");
 Console.WriteLine("Welcome to my humble shop.");
 Console.WriteLine("You got a name don't you?");
 string playername = Console.ReadLine();
@@ -9,22 +9,29 @@ Console.WriteLine($"Ah pleasure to meet ya {playername}.");
 
 // Step 2: Ask player for sum olf held gold
 Console.WriteLine("I see your looking to buy, how much gold are you carrying?");
-string heldgold = Console.ReadLine();
+int heldgold = int.Parse(Console.ReadLine());
 
 //Step 3+: Tell player about sword, do they wish to purchase?
 Console.WriteLine("Well I have a fine nice 'bronze sword' for sale, only 15 gold.");
 Console.WriteLine("Do you wish to purchase it?");
 string dopurchasesword = Console.ReadLine();
-int number1 = 15;
+
 if (dopurchasesword == "yes")
 {
-    if (heldgold >= {number1})
+    if (heldgold >= 15)
     {
-        Console.WriteLine($"{playername} bought 'bronze sword'");
+        Console.WriteLine($"{playername} purchased 'bronze sword'.");
+        heldgold -= 15;
+        Console.WriteLine($"{playername} balance is now {heldgold}.");
     }
-    // inform player of remaining gold
+    else
+    {
+        Console.WriteLine("Sorry but you don't have enough coin, come back when you do.");
+    }
+
 }
+
 else
 {
-    Console.WriteLine($"Thats a shame, stay safe on your travles {playername}");
+    Console.WriteLine($"Thats a shame, stay safe on your journy {playername}");
 }
